78478f3c29b9bad880db98740f02704d8dc417c6
Niti Reddy <nreddy.24@presentationhs.org>
2022-03-22 22:42:23 -0700

    Added auton, teleop, and climb level labels to line graph
    Added a dataToGraph function with graph code and called it in processMatchData

--- teamLookup.php
+++ teamLookup.php
@@ -215,6 +215,8 @@
                 <div class="card mb-3">
                     <div class="card-body">
                         <h5 class="card-title">Graph</h5>
+                        <canvas id="myChart" width="400" height="400"></canvas>
+                        
                         </div>
                 </div>
                 <div class="card mb-3">
@@ -320,8 +322,51 @@
       dataToCommentTable(data);
       dataToMatchTable(data);
       dataToAvgTables(processedData);
+      dataToGraph(data);
       sorttable.makeSortable(document.getElementById("sortableAllMatches"));
+      dataToGraph(data);
     }
+    
+    function dataToGraph(data){
+        const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                    data: {
+                labels: ['Auton Low', 'Auton High', 'Teleop Low', 'Teleop High', 'Climb Level'],
+                        datasets: [{
+                label: 'Average Match Performance',
+                data: [0, 3, 1, 5, 2],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+                
+          
+});
+}
+           
     
     function processCommentData(data){
       dataToCommentTable(data);
@@ -388,6 +433,44 @@
     });
     
     
+</script>
+ <script>
+/* (Graph Code) const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Auton Low', 'Auton High', 'Teleop Low', 'Teleop High', 'Climb Level'],
+        datasets: [{
+            label: 'Average Match Performance',
+            data: [0, 3, 1, 5, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+}); */ 
+     
 </script>
     
 <script type="text/javascript" src="./scripts/matchDataProcessor.js"></script>
