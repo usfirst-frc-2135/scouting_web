d3845ee02fa5903074f35cd14f205b111254f7eb
Jeff Mullins <jeff.l.mullins@gmail.com>
2022-11-16 18:33:29 -0800

    Added auto-format of qrHandler.js

--- scripts/qrHandler.js
+++ scripts/qrHandler.js
@@ -8,54 +8,54 @@ var localStore = window.localStorage;
   Function Definition
 */
 
-function qrStringToList(dataString){
+function qrStringToList(dataString) {
   out = dataString.trim().split("\t");
-  for(var i = 0; i < out.length; ++i){
+  for (var i = 0; i < out.length; ++i) {
     out[i] = out[i].trim();
   }
   return out;
 }
 
-function validateQrList(dataList){
-  if (dataList.length != 13){
+function validateQrList(dataList) {
+  if (dataList.length != 13) {
     return false;
   }
   return true;
 }
 
-function padList(dataList){
-  if(dataList.length == 12){
+function padList(dataList) {
+  if (dataList.length == 12) {
     dataList.push("");
   }
   return dataList;
 }
 
-function qrListToDict(dataList){
+function qrListToDict(dataList) {
   out = {};
-  out["teamnumber"]       = dataList[0];
-  out["startpos"]         = dataList[1];
-  out["tarmac"]           = dataList[2];
-  out["autonlowpoints"]   = dataList[3];
-  out["autonhighpoints"]  = dataList[4];
-  out["teleoplowpoints"]  = dataList[5];
+  out["teamnumber"] = dataList[0];
+  out["startpos"] = dataList[1];
+  out["tarmac"] = dataList[2];
+  out["autonlowpoints"] = dataList[3];
+  out["autonhighpoints"] = dataList[4];
+  out["teleoplowpoints"] = dataList[5];
   out["teleophighpoints"] = dataList[6];
-  out["climbed"]          = dataList[7];
-  out["died"]             = dataList[8];
-  out["matchnumber"]      = dataList[9];
-  out["eventcode"]        = dataList[10];
-  out["scoutname"]        = dataList[11];
-  out["comment"]          = dataList[12];
+  out["climbed"] = dataList[7];
+  out["died"] = dataList[8];
+  out["matchnumber"] = dataList[9];
+  out["eventcode"] = dataList[10];
+  out["scoutname"] = dataList[11];
+  out["comment"] = dataList[12];
   return out;
 }
 
-function qrListToKey(dataObj){
+function qrListToKey(dataObj) {
   return dataObj["eventcode"] + "_" + dataObj["matchnumber"] + "_" + dataObj["teamnumber"];
 }
 
-function addQrData(dataObj){
+function addQrData(dataObj) {
   var key = qrListToKey(dataObj);
-  
-  if (!scannedData.hasOwnProperty(key)){
+
+  if (!scannedData.hasOwnProperty(key)) {
     // Modify global variables
     scannedData[key] = dataObj;
     ++scannedCount;
@@ -68,41 +68,41 @@ function addQrData(dataObj){
         $("<td>").text(dataObj["teamnumber"]),
         $("<td>").text(dataObj["scoutname"]),
         $("<td>").append(
-          "<button id='"+key+"_delete' value='"+key+"' type='button' class='btn btn-danger deleteRowButton'>Delete</button>"
+          "<button id='" + key + "_delete' value='" + key + "' type='button' class='btn btn-danger deleteRowButton'>Delete</button>"
         )
-      ]).prop("id" , key+"_row")
+      ]).prop("id", key + "_row")
     );
     // Add delete button
-    $("#"+key+"_delete").on('click', function(event){
+    $("#" + key + "_delete").on('click', function (event) {
       removeQrData($(this).val());
     });
   }
 }
 
-function removeQrData(dataKey){
-  if (scannedData.hasOwnProperty(dataKey)){
+function removeQrData(dataKey) {
+  if (scannedData.hasOwnProperty(dataKey)) {
     // Modify global variables
     delete scannedData[dataKey];
     --scannedCount;
     $("#submitData").html("Submit Data: " + scannedCount);
     // Modify table
-    $("#"+dataKey+"_row").remove();
-  } 
+    $("#" + dataKey + "_row").remove();
+  }
 }
 
 /*
   Saves default camera ID to localStorage for on reload camera config persistence
 */
-function setDefaultDeviceID(id){
+function setDefaultDeviceID(id) {
   localStorage.setItem("cameraDefaultID", id);
 }
 
 /*
   Reads default camera ID from localStorage, or passes default first ID
 */
-function getDefaultDeviceID(id){
+function getDefaultDeviceID(id) {
   var defaultId = localStorage.getItem("cameraDefaultID");
-  if (defaultId == null){
+  if (defaultId == null) {
     return id;
   }
   else {
@@ -113,13 +113,13 @@ function getDefaultDeviceID(id){
 /*
   Responsible for handling actions that occur when camera is scanning
 */
-function scanCamera(reader, id){
+function scanCamera(reader, id) {
   reader.decodeFromInputVideoDeviceContinuously(id, 'camera', (result, err) => {
     if (result) {
       var dataList = qrStringToList(result.text);
       dataList = padList(dataList);
       console.log(dataList);
-      if (validateQrList(dataList)){
+      if (validateQrList(dataList)) {
         alertSuccessfulScan();
         addQrData(qrListToDict(dataList));
       }
@@ -130,41 +130,40 @@ function scanCamera(reader, id){
   });
 }
 
-
-function alertSuccessfulScan(){
-  try{
+function alertSuccessfulScan() {
+  try {
     window.navigator.vibrate(200);
   }
-  catch(exception){
-    
+  catch (exception) {
+
   }
   $("#content").addClass("bg-success");
-  var timeoutFunction = setTimeout(function(){$("#content").removeClass("bg-success");}, 500);
+  var timeoutFunction = setTimeout(function () { $("#content").removeClass("bg-success"); }, 500);
 }
 
 /*
   Function bound to the QR Reader
   Handles reading the different cameras connected to device
 */
-function createCameraSelect(reader){
+function createCameraSelect(reader) {
   reader.getVideoInputDevices().then((videoInputDevices) => {
-    
+
     // Creates drop down menu to switch between cameras
     var initial_id = null;
     if (videoInputDevices.length >= 1) {
       videoInputDevices.forEach((element) => {
-        if (initial_id == null){
+        if (initial_id == null) {
           initial_id = element.deviceId;
         }
-        $("#cameraSelect").append($('<option>', {value: element.deviceId, text: element.label}));
+        $("#cameraSelect").append($('<option>', { value: element.deviceId, text: element.label }));
       });
     }
-    
+
     // Creates default camera scanner based on saved data
     scanCamera(reader, getDefaultDeviceID(initial_id));
 
     // Binds drop down on change to select new camera when necessary
-    $("#cameraSelect" ).change(function() {
+    $("#cameraSelect").change(function () {
       var selCamID = $("#cameraSelect").val();
       scanCamera(reader, selCamID);
       setDefaultDeviceID(selCamID);
@@ -172,7 +171,7 @@ function createCameraSelect(reader){
   });
 }
 
-function clearData(){
+function clearData() {
   $("#qrValidationTable").html("");
   scannedCount = 0;
   scannedData = {};
@@ -181,14 +180,14 @@ function clearData(){
 /*
   Submit Function
 */
-function submitFunction(){
-  $("#submitData").on('click', function(event){
+function submitFunction() {
+  $("#submitData").on('click', function (event) {
     var indexedData = [];
-    for(const [key, value] of Object.entries(scannedData)){
+    for (const [key, value] of Object.entries(scannedData)) {
       indexedData.push(value);
     }
-    $.post("writeAPI.php", {"writeData" : JSON.stringify(indexedData)}, function(data){
-      if (data == "success"){
+    $.post("writeAPI.php", { "writeData": JSON.stringify(indexedData) }, function (data) {
+      if (data == "success") {
         alert("Data Successfully Submitted! Clearing Data.");
         clearData();
       } else {
@@ -198,11 +197,11 @@ function submitFunction(){
   });
 }
 
-$(document).ready(function() {
-    const reader = new ZXing.BrowserQRCodeReader();
-    
-    createCameraSelect(reader);
-    
-    submitFunction();
-    
+$(document).ready(function () {
+  const reader = new ZXing.BrowserQRCodeReader();
+
+  createCameraSelect(reader);
+
+  submitFunction();
+
 });
