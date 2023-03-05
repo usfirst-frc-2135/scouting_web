034ccb16daaca29fc45b48adffdcc8ef04cf8ab7
Calista Hua <chua.25@presentationhs.org>
2023-03-05 14:27:57 -0800

    shortened the "submit data" button on qr scanner page

--- qrScanner.php
+++ qrScanner.php
@@ -4,13 +4,13 @@
 <div class="container row-offcanvas row-offcanvas-left">
   <div class="well column  col-lg-12  col-sm-12 col-xs-12" id="content">
 
-    <div class="row pt-3 pb-3 mb-3">
-      <button id="submitData" type="button" class="btn btn-success">Submit Data: 0</button>
+    <div class="d-grid col-3 mt-3 mx-auto">
+      <button id="submitData" type="button" class="btn btn-success"> Click to Submit Data: 0</button>
     </div>
 
     <div class="row pt-3 pb-3 mb-3">
       <div id="interactive" class="viewport">
-        <video autoplay="true" id="camera">
+          <video autoplay="true" id="camera"></video>
       </div>
     </div>
 
