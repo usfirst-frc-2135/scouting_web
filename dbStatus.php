ccb214d5f4dab11b016397b1f3c771dd88f6bd89
Jeanne Mullins <jeannekmullins@gmail.com>
2024-10-02 14:33:25 -0700

    Removed FireBase handling from dbStatus page

--- dbStatus.php
+++ dbStatus.php
@@ -22,16 +22,6 @@
             <h4>Username: <span id="userName" class="badge bg-primary">????</span></h4>
             <h4>TBA Key: <span id="tbaKey" class="badge bg-primary">????</span></h4>
             <h4>Event Code: <span id="eventCode" class="badge bg-primary">????</span></h4>
-            <br>
-            <h3>Firebase Config</h3>
-            <h4>Firebase API Key: <span id="writefbapikey" class="badge bg-primary">????</span></h4>
-            <h4>Firebase Auth Domain: <span id="writefbauthdomain" class="badge bg-primary">????</span></h4>
-            <h4>Firebase DB URL: <span id="writefbdburl" class="badge bg-primary">????</span></h4>
-            <h4>Firebase Project ID: <span id="writefbprojectid" class="badge bg-primary">????</span></h4>
-            <h4>Firebase Storage Bucket: <span id="writefbstoragebucket" class="badge bg-primary">????</span></h4>
-            <h4>Firebase Sender ID: <span id="writefbsenderid" class="badge bg-primary">????</span></h4>
-            <h4>Firebase App ID: <span id="writefbappid" class="badge bg-primary">????</span></h4>
-            <h4>Firebase Measurement ID: <span id="writefbmeasurementid" class="badge bg-primary">????</span></h4>
           </div>
         </div>
 
@@ -129,38 +119,7 @@
               <label for="writeEventCode" class="form-label">Event Code</label>
               <input type="text" class="form-control" id="writeEventCode" aria-describedby="tbaEventCode">
             </div>
-            <div class="mb-3">
-              <label for="fbAPIKey" class="form-label">Firebase API Key</label>
-              <input type="text" class="form-control" id="fbAPIKey" aria-describedby="fbAPIKey">
-            </div>
-            <div class="mb-3">
-              <label for="fbAuthDomain" class="form-label">Firebase Authentication Domain</label>
-              <input type="text" class="form-control" id="fbAuthDomain" aria-describedby="fbAuthDomain">
-            </div>
-            <div class="mb-3">
-              <label for="fbDBUrl" class="form-label">Firebase DB URL</label>
-              <input type="text" class="form-control" id="fbDBUrl" aria-describedby="fbDBUrl">
-            </div>
-            <div class="mb-3">
-              <label for="fbProjectID" class="form-label">Firebase Project ID</label>
-              <input type="text" class="form-control" id="fbProjectID" aria-describedby="fbProjectID">
-            </div>
-            <div class="mb-3">
-              <label for="fbStorageBucket" class="form-label">Firebase Storage Bucket</label>
-              <input type="text" class="form-control" id="fbStorageBucket" aria-describedby="fbStorageBucket">
-            </div>
-            <div class="mb-3">
-              <label for="fbSenderID" class="form-label">Firebase Sender ID</label>
-              <input type="text" class="form-control" id="fbSenderID" aria-describedby="fbSenderID">
-            </div>
-            <div class="mb-3">
-              <label for="fbAppID" class="form-label">Firebase Application ID</label>
-              <input type="text" class="form-control" id="fbAppID" aria-describedby="fbAppID">
-            </div>
-            <div class="mb-3">
-              <label for="fbMeasurementID" class="form-label">Firebase Measurement ID</label>
-              <input type="text" class="form-control" id="fbMeasurementID" aria-describedby="fbMeasurementID">
-            </div>
+
             <button id="writeConfig" class="btn btn-primary">Write Config</button>
             <button id="createDB" class="btn btn-primary">Create DB</button>
             <button id="createTable" class="btn btn-primary">Create Table</button>
@@ -213,15 +172,6 @@
     setStatusBadge(statusArray["rankTableExists"], "rankTableStatus");
     setStatusBadge(statusArray["driveRankTableExists"], "driveRankTableStatus");
 	  
-    $("#writefbapikey").text(statusArray["fbapikey"]);
-    $("#writefbauthdomain").text(statusArray["fbauthdomain"]);
-    $("#writefbdburl").text(statusArray["fbdburl"]);
-    $("#writefbprojectid").text(statusArray["fbprojectid"]);
-    $("#writefbstoragebucket").text(statusArray["fbstoragebucket"]);
-    $("#writefbsenderid").text(statusArray["fbsenderid"]);
-    $("#writefbappid").text(statusArray["fbappid"]);
-    $("#writefbmeasurementid").text(statusArray["fbmeasurementid"]);
-
     $("#dataP").prop('checked', statusArray["useP"]);
     $("#dataQm").prop('checked', statusArray["useQm"]);
     $("#dataQf").prop('checked', statusArray["useQf"]);
@@ -242,15 +192,7 @@
     "writeTBATable": "tbatable",
     "writePitTable": "pittable",
     "writeRankTable": "ranktable",
-	"writeDriveRankTable": "driveranktable",
-    "fbAPIKey": "fbapikey",
-    "fbAuthDomain": "fbauthdomain",
-    "fbDBUrl": "fbdburl",
-    "fbProjectID": "fbprojectid",
-    "fbStorageBucket": "fbstoragebucket",
-    "fbSenderID": "fbsenderid",
-    "fbAppID": "fbappid",
-    "fbMeasurementID": "fbmeasurementid"
+    "writeDriveRankTable": "driveranktable",
   };
   var id_to_written_map = {}
 
