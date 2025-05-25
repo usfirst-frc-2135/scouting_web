fd56549ce78576e2f2a33d512904e026c289a2a5
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-05-24 23:16:06 -0700

    Improve photo upload layout

--- qrScanner.php
+++ qrScanner.php
@@ -5,21 +5,15 @@
 <div class="container row-offcanvas row-offcanvas-left">
   <div class="well column  col-lg-12  col-sm-12 col-xs-12" id="content">
 
-    <div class="d-grid col-3 mt-3 mx-auto">
-      <button id="submitData" type="button" class="btn btn-success"> Click to Submit Data: 0</button>
-    </div>
-
     <div class="row pt-3 pb-3 mb-3">
-      <div id="interactive" class="viewport">
-        <video autoplay="true" id="camera"></video>
+      <div class="d-grid col-6 mt-3 mx-auto">
+        <div id="interactive" class="viewport">
+          <video autoplay="true" id="camera"></video>
+        </div>
+        <select id="cameraSelect" class="form-select form-select mb-3" aria-label=".form-select-lg example">
+        </select>
+        <button id="submitData" type="button" class="btn btn-success"> Click to Submit Data: 0</button>
       </div>
-    </div>
-
-    <br>
-
-    <div class="row">
-      <select id="cameraSelect" class="form-select form-select mb-3" aria-label=".form-select-lg example">
-      </select>
     </div>
 
     <table class="table">
