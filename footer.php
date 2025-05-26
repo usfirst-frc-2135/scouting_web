00bd414f5b536bf1114d72298b50c8984a694756
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-05-25 17:52:29 -0700

    Update jquery and jquery-ui

--- footer.php
+++ footer.php
@@ -2,18 +2,19 @@
 
 </footer>
 
-<script type="text/javascript" src="./external/jquery/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="./external/jquery/jquery-3.7.1.min.js"></script>
 <script type="text/javascript" src="./external/bootstrap-5.3.6-dist/js/bootstrap.bundle.min.js"></script>
 <!-- <script type="text/javascript" src="./external/bootstrap-5.3.6-dist/js/bootstrap.min.js"></script> -->
+<script type="text/javascript" src="./external/jquery-ui/jquery-ui.js"></script>
 <script type="text/javascript" src="./external/zxing/zxing.min.js"></script>
 <script type="text/javascript" src="./external/sorttable/sorttable.js"></script>
 <script type="text/javascript" src="./external/charts/chart.min.js"></script>
-<script type="text/javascript" src="./external/jquery-ui/jquery-ui.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
 <!--<script type="module" src="./external/sortable/plugins/AutoScroll/AutoScroll.js"></script>-->
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+<!-- <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script> -->
+<!-- <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script> -->
 <script type="text/javascript" src="./external/freeze-table/freeze-table.js"></script>
+
 </body>
 
 </html>
