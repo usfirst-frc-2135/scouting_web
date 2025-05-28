e6f2ae8ba03114b66bd459633115a33d2b1a5b5d
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-05-28 00:08:56 -0400

    Update variable naming in pit status/index page

--- index.php
+++ index.php
@@ -29,36 +29,35 @@ require 'header.php';
 <?php include 'footer.php'; ?>
 
 <script>
-  var allPitData = {};
   var teamList = [];
-  var picLookup = {};
   var teamNames = {};
-  var eventCode = null;
+  var jsonPitList = {};
+  var jsonImageList = {};
 
   function createTable() {
-    if (allPitData == null || teamList == null) {
+    if (jsonPitList == null || teamList == null) {
       return null;
     }
 
     $("#pitScoutTable").html("");
     var row = "";
     for (let teamNum of teamList) {
-      var teamname = teamNames[teamNum];
+      var teamName = teamNames[teamNum];
       row += "<tr>";
-      if (teamname != "XX") {
-        row += "  <td>" + "<a class='text-black' href='teamLookup.php?teamNum=" + teamNum + "'>" + teamNum + "</a> - " + teamname + "</td>";
+      if (teamName != "XX") {
+        row += "  <td>" + "<a class='text-black' href='teamLookup.php?teamNum=" + teamNum + "'>" + teamNum + "</a> - " + teamName + "</td>";
       } else {
         row += "  <td>" + "<a class='text-black' href='teamLookup.php?teamNum=" + teamNum + "'>" + teamNum + "</a>" + "</td>";
       }
 
-      if (allPitData[teamNum] != null) {
+      if (jsonPitList[teamNum] != null) {
         row += "  <td class='bg-success'>Yes</td>";
       } else {
         row += "  <td>No</td>";
       }
 
-      if (picLookup[teamNum] != null) {
-        if (picLookup[teamNum].length > 0) {
+      if (jsonImageList[teamNum] != null) {
+        if (jsonImageList[teamNum].length > 0) {
           row += "  <td class='bg-success'>Yes</td>";
         } else {
           row += "  <td>No</td>";
@@ -79,22 +78,23 @@ require 'header.php';
   function sortTable() {
     var table = document.getElementById("psTable");
     var rows = Array.prototype.slice.call(table.querySelectorAll("tbody> tr"));
+
     rows.sort(function (rowA, rowB) {
-      //      console.log(" >>>> starting rows.sort()");
+      // console.log(" >>>> starting rows.sort()");
       var cellA = rowA.cells[0].textContent.trim();
-      //      console.log("   >>> cellA = "+cellA);
+      // console.log("   >>> cellA = " + cellA);
       var cellB = rowB.cells[0].textContent.trim();
-      //      console.log("     >>> cellB = "+cellB);
+      // console.log("     >>> cellB = " + cellB);
 
       // Remove the " - <teamName>" from the end of the entry.
-      //      console.log("===> cellA = "+cellA+ "; cellB = "+cellB);
+      // console.log("===> cellA = " + cellA + "; cellB = " + cellB);
       const dashPosA = cellA.indexOf("-");
       if (dashPosA != -1)
         cellA = cellA.substr(0, dashPosA - 1);
       const dashPosB = cellB.indexOf("-");
       if (dashPosB != -1)
         cellB = cellB.substr(0, dashPosB - 1);
-      //      console.log("    ==> after: cellA = "+cellA+ "; cellB = "+cellB);
+      // console.log("    ==> after: cellA = " + cellA + "; cellB = " + cellB);
 
       // Remove any letters at the last char in teamNum for the sort comparison.
       if (cellA.charAt(cellA.length - 1) == "B" || cellA.charAt(cellA.length - 1) == "C" ||
@@ -128,32 +128,32 @@ require 'header.php';
     });
 
     // Get the list of teams and add the team names 
-    console.log("index: getting teamlist from tbaAPI");
+    console.log("index: getting teamlist from tbaAPI using db_config.ini event code");
     $.get("./tbaAPI.php", {
       getTeamListAndNames: 1
     }).done(function (data) {
       if (data == null)
-        alert("Can't load teamlist from TBA; check if TBA Key was set in dbStatus");
+        alert("Can't load teamlist from TBA; check if TBA Key was set in db_config.ini");
       else {
-        var rawTeams = JSON.parse(data);
-        for (var i = 0; i < rawTeams.length; i++) {
-          var teamnum = rawTeams[i]["teamnum"];
-          var teamname = rawTeams[i]["teamname"];
-          teamList.push(teamnum);
-          teamNames[teamnum] = teamname;
+        var jsonTeamList = JSON.parse(data);
+        for (var i = 0; i < jsonTeamList.length; i++) {
+          var teamNum = jsonTeamList[i]["teamnum"];
+          var teamName = jsonTeamList[i]["teamname"];
+          teamList.push(teamNum);
+          teamNames[teamNum] = teamName;
         }
 
         // Get all the team images
         $.get("./readAPI.php", {
           getTeamsImages: JSON.stringify(teamList)
         }).done(function (data) {
-          //          console.log("index.php: getTeamsImages = "+data);
-          picLookup = JSON.parse(data);
+          // console.log("index.php: getTeamsImages = " + data);
+          jsonImageList = JSON.parse(data);
           // Get all the teams pit scouted
           $.get("./readAPI.php", {
             getAllPitData: 1
           }).done(function (data) {
-            allPitData = JSON.parse(data);
+            jsonPitList = JSON.parse(data);
             createTable();
             sorttable.makeSortable(document.getElementById("psTable"));
             sortTable();
