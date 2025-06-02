9c3e71c0ab8ca1115c7d2cb4cda880d761958911
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-06-01 22:01:27 -0700

    Changed navbar items to toggle using bootstrap classes instead of cryptic CSS code

--- header.php
+++ header.php
@@ -16,118 +16,93 @@
 
   <style>
     .form-check-input {
-      border: 1px;
-      border-style: solid;
+      /* darken check box borders */
       border-color: rgba(0, 0, 0, .5);
     }
 
-    .nav .navbar {
-      text-decoration: none;
-      margin: auto;
-      float: none;
-      text-align: center;
-    }
-
-    li {
-      text-decoration: none;
-      margin: auto;
-      float: none;
-      list-style-type: none;
-      overflow: hidden;
-    }
-
-    .nav a {
-      text-decoration: none;
-      margin: auto;
-      float: none;
-      text-align: center;
-      list-style-type: none;
-      display: block;
-      width: 84px;
-      color: #A9A9A9;
-    }
-
-
-    .nav li a:hover {
-      color: #fff;
-    }
-
-    .nav li .selected {
-      text-decoration: none;
-      margin: auto;
-      float: none;
-      text-align: center;
-      list-style-type: none;
-      width: 76px;
-      background-color: #0d6efd;
-      color: #fff;
+    .nav-pills {
+      /* navbar button text should be white */
+      color: "white";
     }
   </style>
 </head>
 
 <body class="bg-light">
   <!-- planning to look somewhere to investigate how far the header expands, see if i can ... idk what it says after-->
-  <header class="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">FRC 2135</a>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <a class="navbar-brand text-light" href="#">FRC 2135</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div id="navbarCollapse" class="collapse navbar-collapse">
-        <ul class="nav" style="text-align:center">
-          <li class="nav-item">
-            <a class="nav-link" href="./index.php">Scouting Status</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./pitForm.php">Pit Scouting</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./pitPhotoUpload.php">Photo Upload</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./matchQrScanner.php">QR Scanner</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./matchData.php">Match Data</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./matchAverages.php">Match Averages</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./strategicSchedule.php">Strategic Schedule</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./strategicForm.php">Strategic Scouting</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./strategicData.php">Strategic Data</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./teamLookup.php">Team Lookup</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./matchSheet.php">Match Sheet</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./eventCoprData.php">Event COPRs</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./databaseStatus.php">Database Status</a>
-          </li>
+        <ul class="navbar-nav mr-auto">
+          <ul class="nav nav-pills nav-justified">
+            <li class="nav-item">
+              <a class="nav-link text-secondary active" data-toggle="pill" href="./index.php">Scouting Status</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./pitForm.php">Pit Scouting</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./pitPhotoUpload.php">Photo Upload</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./matchQrScanner.php">QR Scanner</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./matchData.php">Match Data</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./matchAverages.php">Match Averages</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./strategicSchedule.php">Strategic Schedule</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./strategicForm.php">Strategic Scouting</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./strategicData.php">Strategic Data</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./teamLookup.php">Team Lookup</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./matchSheet.php">Match Sheet</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./eventCoprData.php">Event COPRs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" data-toggle="pill" href="./databaseStatus.php">Database Status</a>
+            </li>
+          </ul>
         </ul>
       </div>
-    </div>
+    </nav>
   </header>
 
   <script>
+    // Handle navbar button state toggles
     const currentLocation = location.href;
     const items = document.querySelectorAll("a");
     const length = items.length;
 
     for (let i = 0; i < items.length; i++) {
       if (items[i].href === currentLocation) {
-        items[i].className = "selected";
+        // alert("add: " + items[i]);
+        items[i].classList.add("active");
+        items[i].classList.add("text-light");
+        items[i].classList.remove("text-secondary");
+      }
+      else {
+        // alert("remove: " + items[i]);
+        items[i].classList.remove("active");
+        items[i].classList.remove("text-light");
+        items[i].classList.add("text-secondary");
       }
     }
   </script>
