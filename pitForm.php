1b752cb56659038824884e0a596cbc4c1697b500
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-06-10 14:38:20 -0700

    Added team number validation to pit form

--- pitForm.php
+++ pitForm.php
@@ -28,115 +28,129 @@ require 'header.php';
               <input id="teamNumber" class="form-control" type="text" placeholder="FRC team number">
             </div>
 
-            <div class="mb-3">
-              <div>
-                <span>Pit Organization</span>
-                <span class="text-danger"> (observe only, do not ask)</span>
+            <div class="card col-md-12 mx-auto">
+              <div class="card-header">
+                <h5>Questions</h5>
               </div>
-              <div class="form-check form-check-inline">
-                <input id="pitScore1" class="form-check-input" type="radio" name="pitOrgGroup">
-                <label for="pitScore1" class="form-check-label">1 (Unorganized)</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="pitScore2" class="form-check-input" type="radio" name="pitOrgGroup">
-                <label for="pitScore2" class="form-check-label">3 (Average)</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="pitScore3" class="form-check-input" type="radio" name="pitOrgGroup">
-                <label for="pitScore3" class="form-check-label">5 (Pristine)</label>
+
+              <div class="card-body">
+                <div class="mb-3">
+                  <div>
+                    <span>Does your robot have swerve drive?</span>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="swerveDriveYes" class="form-check-input" type="radio" name="swerveDriveGroup">
+                    <label for="swerveDriveYes" class="form-check-label">Yes</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="swerveDriveNo" class="form-check-input" type="radio" name="swerveDriveGroup">
+                    <label for="swerveDriveNo" class="form-check-label">No</label>
+                  </div>
+                </div>
+
+                <div class="mb-3">
+                  <span>What type of motors do you use on your drive train?</span>
+                  <div class="input-group mb-3">
+                    <select id="driveMotors" class="form-select">
+                      <option selected>Choose...</option>
+                      <option value="1">Krakens</option>
+                      <option value="2">Neos</option>
+                      <option value="3">Falcons</option>
+                      <option value="4">CIMs</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="mb-3">
+                  <div>
+                    <span>Does your team have spare parts for the robot?</span>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="sparePartsYes" class="form-check-input" type="radio" name="sparePartsGroup">
+                    <label for="sparePartsYes" class="form-check-label">Yes</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="sparePartsNo" class="form-check-input" type="radio" name="sparePartsGroup">
+                    <label for="sparePartsNo" class="form-check-label">No</label>
+                  </div>
+                </div>
+
+                <div class="mb-3">
+                  <span>What programming language do you use?</span>
+                  <div class="input-group mb-3">
+                    <select id="programmingLanguage" class="form-select">
+                      <option id="programmingLanguage" selected value="0">Choose...</option>
+                      <option id="java" value="1">Java</option>
+                      <option id="labView" value="2">LabView</option>
+                      <option id="C++" value="3">C++</option>
+                      <option id="python" value="4">Python</option>
+                      <option id="other" value="5">Other</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="mb-3">
+                  <div>
+                    <span>Does your robot have computer vision?</span>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="computerVisionYes" class="form-check-input" type="radio" name="computerVisionGroup">
+                    <label for="computerVisionYes" class="form-check-label">Yes</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="computerVisionNo" class="form-check-input" type="radio" name="computerVisionGroup">
+                    <label for="computerVisionNo" class="form-check-label">No</label>
+                  </div>
+                </div>
               </div>
             </div>
 
-            <div class="mb-3">
-              <label for="batteries" class="form-label">Count the number of batteries they have</label>
-              <input id="batteries" class="form-control" type="text" placeholder="Battery count">
-            </div>
+            <div class="card col-md-12 mx-auto">
+              <div class="card-header">
+                <h5>Observations<span class="text-danger"> (observe only, do not ask)</span></h5>
+              </div>
 
-            <div class="mb-3">
-              <div>
-                <span>Does your team have spare parts for the robot?</span>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="sparePartsYes" class="form-check-input" type="radio" name="sparePartsGroup">
-                <label for="sparePartsYes" class="form-check-label">Yes</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="sparePartsNo" class="form-check-input" type="radio" name="sparePartsGroup">
-                <label for="sparePartsNo" class="form-check-label">No</label>
-              </div>
-            </div>
+              <div class="card-body">
+                <div class="mb-3">
+                  <div>
+                    <span>Pit Organization</span>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="pitScore1" class="form-check-input" type="radio" name="pitOrgGroup">
+                    <label for="pitScore1" class="form-check-label">1 (Unorganized)</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="pitScore2" class="form-check-input" type="radio" name="pitOrgGroup">
+                    <label for="pitScore2" class="form-check-label">3 (Average)</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="pitScore3" class="form-check-input" type="radio" name="pitOrgGroup">
+                    <label for="pitScore3" class="form-check-label">5 (Pristine)</label>
+                  </div>
+                </div>
 
-            <div class="mb-3">
-              <div>
-                <span>Does your robot have computer vision?</span>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="computerVisionYes" class="form-check-input" type="radio" name="computerVisionGroup">
-                <label for="computerVisionYes" class="form-check-label">Yes</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="computerVisionNo" class="form-check-input" type="radio" name="computerVisionGroup">
-                <label for="computerVisionNo" class="form-check-label">No</label>
-              </div>
-            </div>
+                <div class="mb-3">
+                  <div>
+                    <span>Preparedness/Professionalism</span>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="preparednessScore1" class="form-check-input" type="radio" name="preparednessGroup">
+                    <label for="preparednessScore1" class="form-check-label">1 (Minimal)</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="preparednessScore2" class="form-check-input" type="radio" name="preparednessGroup">
+                    <label for="preparednessScore2" class="form-check-label">3 (Average)</label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input id="preparednessScore3" class="form-check-input" type="radio" name="preparednessGroup">
+                    <label for="preparednessScore3" class="form-check-label">5 (Excellent)</label>
+                  </div>
+                </div>
 
-            <div class="mb-3">
-              <div>
-                <span>Does your robot have swerve drive?</span>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="swerveDriveYes" class="form-check-input" type="radio" name="swerveDriveGroup">
-                <label for="swerveDriveYes" class="form-check-label">Yes</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="swerveDriveNo" class="form-check-input" type="radio" name="swerveDriveGroup">
-                <label for="swerveDriveNo" class="form-check-label">No</label>
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <span>What programming language do you use?</span>
-              <div class="input-group mb-3">
-                <select id="programmingLanguage" class="form-select">
-                  <option id="programmingLanguage" selected value="0">Choose...</option>
-                  <option id="java" value="1">Java</option>
-                  <option id="labView" value="2">LabView</option>
-                  <option id="C++" value="3">C++</option>
-                  <option id="python" value="4">Python</option>
-                  <option id="other" value="5">Other</option>
-                </select>
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <span>What type of motors do you use on your drive train?</span>
-              <div class="input-group mb-3">
-                <select id="driveMotors" class="form-select">
-                  <option selected>Choose...</option>
-                  <option value="1">Krakens</option>
-                  <option value="2">Neos</option>
-                  <option value="3">Falcons</option>
-                  <option value="4">CIMs</option>
-                </select>
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <div>
-                <span>Preparedness/Professionalism</span>
-                <span class="text-danger"> (observe only, do not ask)</span>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="preparednessScore1" class="form-check-input" type="radio" name="preparednessGroup">
-                <label for="preparednessScore1" class="form-check-label">1 (Minimal)</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="preparednessScore2" class="form-check-input" type="radio" name="preparednessGroup">
-                <label for="preparednessScore2" class="form-check-label">3 (Average)</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input id="preparednessScore3" class="form-check-input" type="radio" name="preparednessGroup">
-                <label for="preparednessScore3" class="form-check-label">5 (Excellent)</label>
+                <div class="mb-3">
+                  <label for="batteries" class="form-label">Count the number of batteries they have</label>
+                  <input id="batteries" class="form-control" type="text" placeholder="Battery count">
+                </div>
               </div>
             </div>
 
@@ -160,31 +174,11 @@ require 'header.php';
     console.log("==> pitForm.php: verifyPitForm()");
     var isError = false;
     var errMsg = "Please enter values for these fields:";
+    var teamNum = $("#teamNumber").val();
 
     // Make sure each piece of data has a value selected.
-    if ($("#teamNumber").val() == "") {
+    if ((($("#teamNumber").val() == "") || (validateTeamNumber(teamNum, null) <= 0))) {
       errMsg += " Team Number";
-      isError = true;
-    }
-
-    if ((!($("#pitScore1").is(':checked'))) && (!($("#pitScore2").is(':checked'))) && (!($("#pitScore3").is(':checked')))) {
-      if (isError == true)
-        errMsg += ", Pit Organization";
-      else errMsg += " Pit Organization";
-      isError = true;
-    }
-
-    if ((!($("#sparePartsYes").is(':checked'))) && (!($("#sparePartsNo").is(':checked')))) {
-      if (isError == true)
-        errMsg += ", Spare Parts";
-      else errMsg += " Spare Parts";
-      isError = true;
-    }
-
-    if ((!($("#computerVisionYes").is(':checked'))) && (!($("#computerVisionNo").is(':checked')))) {
-      if (isError == true)
-        errMsg += ", Computer Vision";
-      else errMsg += " Computer Vision";
       isError = true;
     }
 
@@ -192,6 +186,21 @@ require 'header.php';
       if (isError == true)
         errMsg += ", Swerve Drive";
       else errMsg += " Swerve Drive";
+      isError = true;
+    }
+
+    var driveMotors = $("#driveMotors").val();
+    if (driveMotors != 1 && driveMotors != 2 && driveMotors != 3 && driveMotors != 4) {
+      if (isError == true)
+        errMsg += ", Drive Motors";
+      else errMsg += " Drive Motors";
+      isError = true;
+    }
+
+    if ((!($("#sparePartsYes").is(':checked'))) && (!($("#sparePartsNo").is(':checked')))) {
+      if (isError == true)
+        errMsg += ", Spare Parts";
+      else errMsg += " Spare Parts";
       isError = true;
     }
 
@@ -203,11 +212,17 @@ require 'header.php';
       isError = true;
     }
 
-    var driveMotors = $("#driveMotors").val();
-    if (driveMotors != 1 && driveMotors != 2 && driveMotors != 3 && driveMotors != 4) {
+    if ((!($("#computerVisionYes").is(':checked'))) && (!($("#computerVisionNo").is(':checked')))) {
       if (isError == true)
-        errMsg += ", Drive Motors";
-      else errMsg += " Drive Motors";
+        errMsg += ", Computer Vision";
+      else errMsg += " Computer Vision";
+      isError = true;
+    }
+
+    if ((!($("#pitScore1").is(':checked'))) && (!($("#pitScore2").is(':checked'))) && (!($("#pitScore3").is(':checked')))) {
+      if (isError == true)
+        errMsg += ", Pit Organization";
+      else errMsg += " Pit Organization";
       isError = true;
     }
 
@@ -227,54 +242,29 @@ require 'header.php';
   function clearPitForm() {
     console.log("==> pitForm.php: clearPitForm()");
     $("#teamNumber").val("");
-    $("#batteries").val("");
-    $("#preloadAndLeave").val("0");
-    $("#programmingLanguage").val("0");
+    $("#swerveDriveYes").prop("checked", false);
+    $("#swerveDriveNo").prop("checked", false);
     $("#driveMotors").val("0");
+    $("#sparePartsYes").prop("checked", false);
+    $("#sparePartsNo").prop("checked", false);
+    $("#programmingLanguage").val("0");
+    $("#computerVisionYes").prop("checked", false);
+    $("#computerVisionNo").prop("checked", false);
     $("#pitScore1").prop("checked", false);
     $("#pitScore2").prop("checked", false);
     $("#pitScore3").prop("checked", false);
-    $("#sparePartsYes").prop("checked", false);
-    $("#sparePartsNo").prop("checked", false);
-    $("#computerVisionYes").prop("checked", false);
-    $("#computerVisionNo").prop("checked", false);
-    $("#swerveDriveYes").prop("checked", false);
-    $("#swerveDriveNo").prop("checked", false);
     $("#preparednessScore1").prop("checked", false);
     $("#preparednessScore2").prop("checked", false);
     $("#preparednessScore3").prop("checked", false);
+    $("#batteries").val("");
   }
 
   function writeFormToPitTable() {
     console.log("==> pitForm.php: writeFormToPitTable()");
     var dataToUse = {};
     dataToUse["teamnumber"] = $("#teamNumber").val();
-    dataToUse["numbatteries"] = $("#batteries").val();
 
-    if ($("#pitScore1").is(':checked')) {
-      dataToUse["pitorg"] = 1;
-    }
-    if ($("#pitScore2").is(':checked')) {
-      dataToUse["pitorg"] = 3;
-    }
-    if ($("#pitScore3").is(':checked')) {
-      dataToUse["pitorg"] = 5;
-    }
-
-    if ($("#sparePartsYes").is(':checked')) {
-      dataToUse["spareparts"] = 1;
-    }
-    if ($("#sparePartsNo").is(':checked')) {
-      dataToUse["spareparts"] = 0;
-    }
-
-    if ($("#computerVisionYes").is(':checked')) {
-      dataToUse["computervision"] = 1;
-    }
-    if ($("#computerVisionNo").is(':checked')) {
-      dataToUse["computervision"] = 0;
-    }
-
+    // Swerve
     if ($("#swerveDriveYes").is(':checked')) {
       dataToUse["swerve"] = 1;
     }
@@ -282,6 +272,30 @@ require 'header.php';
       dataToUse["swerve"] = 0;
     }
 
+    // Drive motors
+    var driveMotors = $("#driveMotors").val()
+    if (driveMotors == 1) {
+      dataToUse["drivemotors"] = "Krakens";
+    }
+    if (driveMotors == 2) {
+      dataToUse["drivemotors"] = "NEOs";
+    }
+    if (driveMotors == 3) {
+      dataToUse["drivemotors"] = "Falcons";
+    }
+    if (driveMotors == 4) {
+      dataToUse["drivemotors"] = "CIMs";
+    }
+
+    // Spare parts
+    if ($("#sparePartsYes").is(':checked')) {
+      dataToUse["spareparts"] = 1;
+    }
+    if ($("#sparePartsNo").is(':checked')) {
+      dataToUse["spareparts"] = 0;
+    }
+
+    // Software language
     var progLang = $("#programmingLanguage").val();
     dataToUse["proglanguage"] = "Other";  // default
     if (progLang == 1) {
@@ -300,20 +314,26 @@ require 'header.php';
       dataToUse["proglanguage"] = "Other";
     }
 
-    var driveMotors = $("#driveMotors").val()
-    if (driveMotors == 1) {
-      dataToUse["drivemotors"] = "Krakens";
+    // Computer vision
+    if ($("#computerVisionYes").is(':checked')) {
+      dataToUse["computervision"] = 1;
     }
-    if (driveMotors == 2) {
-      dataToUse["drivemotors"] = "NEOs";
-    }
-    if (driveMotors == 3) {
-      dataToUse["drivemotors"] = "Falcons";
-    }
-    if (driveMotors == 4) {
-      dataToUse["drivemotors"] = "CIMs";
+    if ($("#computerVisionNo").is(':checked')) {
+      dataToUse["computervision"] = 0;
     }
 
+    // Pit organization
+    if ($("#pitScore1").is(':checked')) {
+      dataToUse["pitorg"] = 1;
+    }
+    if ($("#pitScore2").is(':checked')) {
+      dataToUse["pitorg"] = 3;
+    }
+    if ($("#pitScore3").is(':checked')) {
+      dataToUse["pitorg"] = 5;
+    }
+
+    // Overall readiness
     dataToUse["preparedness"] = 1;  // default
     if ($("#preparednessScore1").is(':checked')) {
       dataToUse["preparedness"] = 1;
@@ -324,6 +344,9 @@ require 'header.php';
     else if ($("#preparednessScore3").is(':checked')) {
       dataToUse["preparedness"] = 5;
     }
+
+    // Battery count
+    dataToUse["numbatteries"] = $("#batteries").val();
 
     $.post("api/writeAPI.php", {
       writePitTable: JSON.stringify(dataToUse)
@@ -360,3 +383,5 @@ require 'header.php';
 
   });
 </script>
+
+<script src="./scripts/validateTeamNumber.js"></script>
