512edf41159a3d8896c864abadeaf3cf6f7ea0d4
Jeff Mullins <jeff.l.mullins@gmail.com>
2025-06-10 11:45:33 -0700

    Fixed match number comparison script bugs

--- scripts/compareMatchNumbers.js
+++ scripts/compareMatchNumbers.js
@@ -6,7 +6,7 @@
   Function Definition
 */
 
-//  Compare two alphanumeric match numbers in the form of [comp_level][match_num]
+//  Compare two alphnumAeric match numbers in the form of [comp_level][match_num]
 //    (e.g. p1, qm1, qm43, sf1, etc.)
 //  This function is passed into a sort() to compare two rows. It can convert objects
 //  to strings, trim whitespace, and remove case differences.
@@ -20,57 +20,63 @@ function compareMatchNumbers(matchA, matchB) {
   // console.log("==> compareMatchNumbers: " + matchA + " <-> " + matchB);
 
   // Normalize input parameters
-  var aPrefix = "";
-  var bPrefix = "";
-  var aNum = "";
-  var bNum = "";
+  var prefixA = "";
+  var prefixB = "";
+  var numA = "";
+  var numB = "";
   matchA = matchA.toString().trim().toLowerCase();
   matchB = matchB.toString().trim().toLowerCase();
 
   // Pull apart prefix and number from matchnum (ie, "p", "qm", "sf")
   if (matchA.charAt(0) == "p") {
-    aNum = matchA.substr(1, matchA.length);
-    aPrefix = "p";
+    numA = matchA.substr(1, matchA.length);
+    prefixA = "p";
   }
   else if (matchA.charAt(0) == "q") {   // "qm"
-    aNum = matchA.substr(2, matchA.length);
-    aPrefix = "qm";
+    numA = matchA.substr(2, matchA.length);
+    prefixA = "qm";
   }
   else if (matchA.charAt(0) == "s") {   // "sf"
-    aNum = matchA.substr(2, matchA.length);
-    aPrefix = "sf";
+    numA = matchA.substr(2, matchA.length);
+    prefixA = "sf";
   }
-  if (aPrefix == "") {
+  if (prefixA == "") {
     console.warn("compareMatchNumbers: matchA is missing comp_level! - " + matchA)
-    aPrefix = "qm";
+    prefixA = "qm";
+    numA = matchA;
+    matchA = prefixA + matchA;
   }
 
   if (matchB.charAt(0) == "p") {
-    bNum = matchB.substr(1, matchB.length);
-    bPrefix = "p";
+    numB = matchB.substr(1, matchB.length);
+    prefixB = "p";
   }
   else if (matchB.charAt(0) == "q") {   // "qm"
-    bNum = matchB.substr(2, matchB.length);
-    bPrefix = "qm";
+    numB = matchB.substr(2, matchB.length);
+    prefixB = "qm";
   }
   else if (matchB.charAt(0) == "s") {   // "sf"
-    bNum = matchB.substr(2, matchB.length);
-    bPrefix = "sf";
+    numB = matchB.substr(2, matchB.length);
+    prefixB = "sf";
   }
-  if (bPrefix == "") {
+  if (prefixB == "") {
     console.warn("compareMatchNumbers: matchB is missing comp_level! - " + matchB)
-    aPrefix = "qm";
+    prefixB = "qm";
+    numB = matchB;
+    matchB = prefixB + matchB;
   }
+
+  // console.log("compareMatchNumbers: " + prefixA + " " + numA + " " + matchA + " " + prefixB + " " + numB + " " + matchB);
 
   var returnVal;
 
-  if (aPrefix == bPrefix) // Comp level is same, use numbers
-    returnVal = (aNum - bNum);
-  else if (aPrefix == "p")     // A != B, practice matches always first
+  if (prefixA == prefixB)   // Comp level is same, use numbers
+    returnVal = (numA - numB);
+  else if (prefixA == "p")  // A != B, practice matches always first
     returnVal = -1;
-  else if (bPrefix == "p")     // A !practice, so B is first
+  else if (prefixB == "p")  // A !practice, so B is first
     returnVal = 1;
-  else if (aPrefix == "qm")    // A & B !practice, so A must be first
+  else if (prefixA == "qm") // A & B !practice, so A must be first
     returnVal = -1;
   else
     returnVal = 1;          // B must be first
